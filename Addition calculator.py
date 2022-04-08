30e3378b624b9379b6a1c63d3bf725d0355c5f26
Phillip Luckiewicz <Phillipluckiewicz@gmail.com>
2022-04-07 20:20:17 -0700

    adding my first practice scripts

---
+++ Addition calculator.py
@@ -0,0 +1,9 @@
+# int = whole numbers
+# float = decimals
+
+
+print("Lets develop a simple addition calculator!")
+first_number = float(input("What is your first number? "))
+second_number = float(input("What is your second number? "))
+sum = first_number + second_number
+print("sum: " +str(sum))
---
+++ first_script.py
@@ -0,0 +1,17 @@
+# Playing around with some things I've learned.
+
+
+first_name = "Phillip"
+last_name = "Luckiewicz"
+age = 28
+new_student = True
+
+if new_student:
+    new_student = " is a new student. "
+else:
+    new_student = " is not a new student. "
+
+print(first_name, last_name + str(new_student) + "He is " + str(age) + " years old.")
+print("This was a test script.")
+print("Hello")
+print("testing auto save")
